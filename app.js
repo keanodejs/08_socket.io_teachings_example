d03d3f8484e858a7889ee62ae58e125445945a63
Claus Bové <clbo@kea.dk>
2016-10-17 21:16:21 +0200

    ex2 initial

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-// Start out by creating a http web server
+// Read a html file and display it in the browser
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-	res.write(200, {'Content-type' : 'text/html'});
-	res.end('Hello World');
+var server = http.createServer(function(req, res) {
+
+    fs.readFile('index.html', 'utf8', function(index) {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end('Hello World');
+    });
 });
 
-server.listen(3000, function () {
-	process.stdout.write('Listening at port 3000 for incomming requests \n');
+server.listen(3000, function() {
+    process.stdout.write('Listening at port 3000 for incomming requests \n');
 });
 
-process.stdin.on('data', function(data){
-	process.stdout.write(data + '\n');
-});
+// a second listener (you can have many)
+//	process.stdin.on('data', function(data){
+//		process.stdout.write(data + '\n');
+//	});
