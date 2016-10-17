0cf197b5362396398c89511ef0587cb9a7ba94f6
Claus Bové <clbo@kea.dk>
2016-10-17 22:23:19 +0200

    initial ex3

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-// Read a html file and display it in the browser
+// Use the socket.io module
 var http = require('http');
 var fs = require('fs');
 
+var index = fs.readFileSync(__dirname + '/index2.html');
+
 var server = http.createServer(function(req, res) {
 
-    fs.readFile('index.html', 'utf8', function(index) {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end('Hello World');
-    });
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(index);
+
 });
 
-server.listen(3000, function() {
+// Implementing the Socket.io part
+// Socket.io server listens to our app
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket) {
+    console.log('a user connected with id: ' + socket.id);
+});
+
+server.listen(3002, function() {
     process.stdout.write('Listening at port 3000 for incomming requests \n');
 });
-
-// a second listener (you can have many)
-//	process.stdin.on('data', function(data){
-//		process.stdout.write(data + '\n');
-//	});
